6b68bde82622810c059d7d0d30332102927df775
Edison <edison@idiotic.solutions>
2026-06-30 02:41:54 -0700

    chore: spike vite migration

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: './src/setupTests.js',
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+  },
+});
